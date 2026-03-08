d9d333daa6bf8db0b933eb000a6b9c0e575dc283
Thanh-Hoa2009 <phanhoa230310@gmail.com>
2026-03-08 11:08:10 +0700

    Create script2.user.js

---
+++ script2.user.js
@@ -0,0 +1,33 @@
+// @name         luaga
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  luaga
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+'use strict';
+
+function removeCaptchaDisable() {
+
+    const element = document.getElementById("invisibleCaptchaShortlink");
+
+    if (element && element.hasAttribute("disabled")) {
+        element.removeAttribute("disabled");
+        console.log("Đã bỏ disabled nút");
+    }
+
+}
+
+window.addEventListener("load", removeCaptchaDisable);
+
+const observer = new MutationObserver(removeCaptchaDisable);
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
+
+})();
